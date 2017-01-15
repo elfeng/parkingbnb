5de2dbd1f0e908e00a71c0a3263a446c9e18f126
Evan Lee <evan.taurusta@gmail.com>
2017-01-15 01:21:44 -0800

    added backend classes

--- app/src/main/java/com/example/evanlee/parkingbnb/homepage.java
+++ app/src/main/java/com/example/evanlee/parkingbnb/homepage.java
@@ -7,7 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.microsoft.windowsazure.mobileservices.*;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
+
+import java.net.MalformedURLException;
+
 public class homepage extends AppCompatActivity {
+
+    private MobileServiceClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,7 @@ public class homepage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +32,24 @@ public class homepage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        try {
+            mClient = new MobileServiceClient("https://parkingbnb.azurewebsites.net", this);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        final TodoItem item = new TodoItem();
+        item.Text = "Awesome item";
+        mClient.getTable(TodoItem.class).insert(item, new TableOperationCallback<TodoItem>() {
+            public void onCompleted(TodoItem entity, Exception exception, ServiceFilterResponse response) {
+                if (exception == null) {
+                    // Insert succeeded
+                } else {
+                    // Insert failed
+                }
+            }
+        });
     }
+
 
 }
